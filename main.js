e83d2b00218a6257833982a24342875c89de98e5
takasa5 <vipqiv00@gmail.com>
2018-09-07 00:06:49 +0900

    JS refactoring

--- main.js
+++ main.js
@@ -2,8 +2,15 @@ $(function(){
 
 });
 
-function onProducts() {
-    $("#article-3").animate({
-        left: "-=" + $("#article-3").width() + "px"
+function onHole(elm) {
+    var page = $(elm).parents("article");
+    openArticle(page, elm.id);
+}
+
+function openArticle(page, content) {
+    width = $(page).width() - 15;
+    $(page).animate({
+        left: "-=" + width + "px"
     });
+    $(page).prev().find("#" + content).show();
 }
