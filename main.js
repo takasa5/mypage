6cb67f6900c6f949cfc4791b59b8733844a5b085
takasago <vipqiv00@gmail.com>
2019-05-01 15:02:34 +0900

    ADD: works

---
+++ main.js
@@ -0,0 +1,10 @@
+var vm = new Vue({
+    el: "#v-dom",
+    data: {
+        show: false,
+        works: works,
+        currentPage: 1,
+        thinWidth: window.innerWidth < 800,
+    }
+})
+
---
+++ works.js
@@ -0,0 +1,131 @@
+var works = [
+    {
+        "title": "Orbit",
+        "icon": null,
+        "start": "2017/08",
+        "abstract": "\"軌道\"でリズムを作るWebアプリ",
+        "abstract-en": "Make the rhythm by \"Orbits\"",
+        "languages": [
+            "jQuery"
+        ],
+        "screenshot": "img/orbit_scr.png",
+        "links": [
+            {
+                "title": "Orbit",
+                "url": "https://takasa5-orbit.herokuapp.com/"
+            },
+            {
+                "title": "GitHub",
+                "url": "https://github.com/takasa5/orbit"
+            }
+        ]
+    },
+    {
+        "title": "stardust",
+        "icon": null,
+        "start": "2017/10",
+        "abstract": "星空の写真から星座を検出",
+        "abstract-en": "Detect constellations from image",
+        "languages": [
+            "Python"
+        ],
+        "screenshot": "img/stardust_result.JPG",
+        "links": [
+            {
+                "title": "GitHub",
+                "url": "https://github.com/takasa5/stardust"
+            }
+        ]
+    },
+    {
+        "title": "michibiki",
+        "icon": "img/michibiki.svg",
+        "start": "2018/01",
+        "abstract": "stardustを誰にでも使いやすく",
+        "abstract-en": "Services of my \"stardust\"",
+        "languages": [
+            "Python",
+            "jQuery"
+        ],
+        "screenshot": "img/michibiki_web_src.png",
+        "links": [
+            {
+                "title": "GitHub",
+                "url": "https://github.com/takasa5/michibiki"
+            },
+            {
+                "title": "LINE bot",
+                "url": "https://line.me/R/ti/p/%40njl8027x"
+            }
+        ]
+    },
+    {
+        "title": "Dooune",
+        "icon": null,
+        "start": "2018/06",
+        "abstract": "落書きから生まれる音楽",
+        "abstract-en": "Tone from doodle",
+        "languages": [
+            "jQuery"
+        ],
+        "screenshot": "img/dooune_scr_2.png",
+        "links": [
+            {
+                "title": "GitHub",
+                "url": "https://github.com/takasa5/dooune"
+            }
+        ]
+    },
+    {
+        "title": "Pudding",
+        "icon": "img/pudding.svg",
+        "start": "2018/08",
+        "abstract": "LINEグループの拡張",
+        "abstract-en": "Extension of LINE Group",
+        "languages": [
+            "Python",
+            "jQuery"
+        ],
+        "screenshot": "img/pudding_scr.JPG",
+        "links": [
+            {
+                "title": "blog",
+                "url": "https://takasa-5.blogspot.com/2018/09/line-bot.html"
+            },
+            {
+                "title": "LINE BOOT AWARDS",
+                "url": "https://www.line-community.me/awards/workdetail/5b8f6e75851f74fd6e0142eb"
+            },
+            {
+                "title": "LINE bot",
+                "url": "https://line.me/R/ti/p/%40sjd1151b"
+            }
+        ]
+    },
+    {
+        "title": "RADOME",
+        "icon": "img/radome.svg",
+        "start": "2018/10",
+        "abstract": "LINEで距離依存匿名チャット",
+        "abstract-en": "Distance-based anonymous chat",
+        "languages": [
+            "Python",
+            "jQuery"
+        ],
+        "screenshot": "img/radome_scr.JPG",
+        "links": [
+            {
+                "title": "blog",
+                "irl": "https://takasa-5.blogspot.com/2018/10/line-bot-2.html"
+            },
+            {
+                "title": "LINE BOOT AWARDS",
+                "url": "https://www.line-community.me/awards/workdetail/5bb429b4851f74632343cc5d"
+            },
+            {
+                "title": "LINE bot",
+                "url": "https://line.me/R/ti/p/%40tli5989n"
+            }
+        ]
+    }
+]
